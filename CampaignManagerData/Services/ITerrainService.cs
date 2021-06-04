7d91fb701ad1c2faa8322beab420e854269935de
Jack Dewhirst <jackdewhirst@gmail.com>
2021-06-04 14:17:05 +0100

    Create ITerrainService and TerrainService

---
+++ CampaignManagerData/Services/ITerrainService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignManagerData
+{
+    public interface ITerrainService
+    {
+        public string GetTerrainDescription(string terrainId);
+
+        public int GetTerrainSpeed(string terrainId);
+
+        public void CreateTerrain(string terrainId, string terrainDescription, int travelSpeed);
+    }
+}
---
+++ CampaignManagerData/Services/TerrainService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignManagerData
+{
+    public class TerrainService : ITerrainService
+    {
+        private readonly DnDCampaignManagerContext _context;
+        public TerrainService(DnDCampaignManagerContext context)
+        {
+            _context = context;
+        }
+        public TerrainService()
+        {
+            _context = new DnDCampaignManagerContext();
+        }
+
+        public string GetTerrainDescription(string terrainId)
+        {
+            return _context.TerrainDetails.Where(td => td.TerrainId == terrainId).FirstOrDefault().TerrainDescription;
+        }
+
+        public int GetTerrainSpeed(string terrainId)
+        {
+            return _context.TerrainDetails.Where(td => td.TerrainId == terrainId).FirstOrDefault().TerrainTravelSpeed;
+        }
+
+        public void CreateTerrain(string terrainId, string terrainDescription, int travelSpeed)
+        {
+            var newTerrain = new TerrainDetail
+            {
+                TerrainId = terrainId,
+                TerrainDescription = terrainDescription,
+                TerrainTravelSpeed = travelSpeed
+            };
+            _context.TerrainDetails.Add(newTerrain);
+            _context.SaveChanges();
+
+        }
+    }
+}
