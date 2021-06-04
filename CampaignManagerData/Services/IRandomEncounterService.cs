b563a25ec3702ca15fdab66f7e71cc6d603dea8e
Jack Dewhirst <jackdewhirst@gmail.com>
2021-06-04 15:06:49 +0100

    Add IRandomEncounterService and RandomEncounterService

---
+++ CampaignManagerData/Services/IRandomEncounterService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignManagerData
+{
+    public interface IRandomEncounterService
+    {
+        private void CheckDiceFormat(string dice) { }
+        public void CreateTable(RandomEncounter r);
+        public void RemoveTable(RandomEncounter r);
+
+        public RandomEncounter GetTableById(string tableId);
+
+    }
+}
---
+++ CampaignManagerData/Services/RandomEncounterService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignManagerData
+{
+    public class RandomEncounterService : IRandomEncounterService
+    {
+        private readonly DnDCampaignManagerContext _context;
+        public RandomEncounterService(DnDCampaignManagerContext context)
+        {
+            _context = context;
+        }
+        public RandomEncounterService()
+        {
+            _context = new DnDCampaignManagerContext();
+        }
+
+        public void CreateTable(RandomEncounter r)
+        {
+            _context.RandomEncounters.Add(r);
+            _context.SaveChanges();
+        }
+
+        public void RemoveTable(RandomEncounter r)
+        {
+            _context.RandomEncounters.Remove(r);
+            _context.SaveChanges();
+        }
+
+        public RandomEncounter GetTableById(string tableId)
+        {
+            return _context.RandomEncounters.Where(re => re.RandEncounterTableId == tableId).FirstOrDefault();
+        }
+    }
+}
