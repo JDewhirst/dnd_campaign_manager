155aee19f159fa4cfc44f4ccba43a8fec972bc0f
Jack Dewhirst <jackdewhirst@gmail.com>
2021-06-04 10:38:57 +0100

    Add province service class

--- CampaignManagerData/Services/IProvinceService.cs
+++ CampaignManagerData/Services/IProvinceService.cs
@@ -10,12 +10,15 @@ namespace CampaignManagerData
     {
         public int GetNumberOfProvinces();
         public List<Province> GetAllProvincesQuery();
-        public string GetProvinceHiddenFeature(string provinceName);
-        public string GetProvinceObviousFeature(string provinceName);
-        public string GetProvinceTravelSpeed(string provinceName);
-        public void UpdateObviousFeatureDescription(string provinceName, string featureText);
-        public void UpdateHiddenFeatureDescription(string provinceName, string featureText);
-        public List<Object> GetProvinceRandomEncounterDetails(string provinceName);
-        public void SetRandomEncounterTable(string provinceName, string tableId);
+
+        public Province GetProvinceByName(string provinceName);
+        public void SaveProvinceChanges();
+        //public string GetProvinceHiddenFeature(string provinceName);
+        //public string GetProvinceObviousFeature(string provinceName);
+        //public string GetProvinceTravelSpeed(string provinceName);
+        //public void UpdateObviousFeatureDescription(string provinceName, string featureText);
+        //public void UpdateHiddenFeatureDescription(string provinceName, string featureText);
+        //public List<Object> GetProvinceRandomEncounterDetails(string provinceName);
+        //public void SetRandomEncounterTable(string provinceName, string tableId);
     }
 }
---
+++ CampaignManagerData/Services/ProvinceService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignManagerData
+{
+    public class ProvinceService : IProvinceService
+    {
+        private readonly DnDCampaignManagerContext _context;
+        public ProvinceService(DnDCampaignManagerContext context)
+        {
+            _context = context;
+        }
+        public ProvinceService()
+        {
+            _context = new DnDCampaignManagerContext();
+        }
+
+        public int GetNumberOfProvinces()
+        {
+            return _context.Provinces.Count();
+        }
+
+        public List<Province> GetAllProvincesQuery()
+        {
+            return _context.Provinces.ToList();
+        }
+
+        public Province GetProvinceByName(string provinceName)
+        {
+            return _context.Provinces.Where(p => p.ProvinceName == provinceName).FirstOrDefault();
+        }
+
+        public void SaveProvinceChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
