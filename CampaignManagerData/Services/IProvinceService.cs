727461e78f6eceb5b502da99f143e93938230ca5
Jack Dewhirst <jackdewhirst@gmail.com>
2021-06-04 10:30:14 +0100

    Add ProvinceService interface

---
+++ CampaignManagerData/Services/IProvinceService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignManagerData
+{
+    public interface IProvinceService
+    {
+        public int GetNumberOfProvinces();
+        public List<Province> GetAllProvincesQuery();
+        public string GetProvinceHiddenFeature(string provinceName);
+        public string GetProvinceObviousFeature(string provinceName);
+        public string GetProvinceTravelSpeed(string provinceName);
+        public void UpdateObviousFeatureDescription(string provinceName, string featureText);
+        public void UpdateHiddenFeatureDescription(string provinceName, string featureText);
+        public List<Object> GetProvinceRandomEncounterDetails(string provinceName);
+        public void SetRandomEncounterTable(string provinceName, string tableId);
+    }
+}
